7dc6e41cc756ae989bb9a475ca6300c3e19da48b
vsolis2706 <vsolis2706@gmail.com>
2021-06-07 02:44:28 -0500

    Update Routes.js

--- admin/src/Routes.js
+++ admin/src/Routes.js
@@ -19,22 +19,9 @@ import EditarProducto from "./views/EditarProducto"
 function Routes() {
     return (
         <div>
-<<<<<<< HEAD
-         <Route exact path="/" component={Home}/>   
-         <Route exact path="/ListaProductos" component={ListaProductos}/> 
-         <Route exact path="/crearproducto" component={CrearProducto}/>    
-         <Route exact path="/editarproducto/:id" component={EditarProducto}/> 
-         <Route exact path="/ListarCategoria" component={ListarCategoria}/>   
-         <Route exact path="/crearcategoria" component={CrearCategoria}/>  
-         <Route exact path="/editarcategoria/:id" component={EditarCategoria}/>  
-         <Route exact path="/ListaClientes" component={ListaClientes}/>   
-         
-         <Route component = {NoMatch} />
-=======
            <Route exact path="/listadeproductos" component={ListaProductos}/>
            <Route exact path="/crearproducto" component={CrearProducto}/>
            <Route exact path="/editarproducto/:id" component={EditarProducto}/>
->>>>>>> 1fa6c7c6b714c3a53758dd7c048034a54048e6be
         </div>
     )
 }
