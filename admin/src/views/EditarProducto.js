eae1120ba34f5097727285fbb0046f505d2ad20d
vsolis2706 <vsolis2706@gmail.com>
2021-06-07 15:54:29 -0500

    Update EditarProducto.js

--- admin/src/views/EditarProducto.js
+++ admin/src/views/EditarProducto.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, {useState, useEffect}  from 'react'
 import {useParams} from "react-router-dom"
 import {editarProducto, obtenerProductoPorId} from "../services/productoService"
@@ -86,35 +85,6 @@ const getProducto = async() =>{
             manejarSubmit = {manejarSubmit}
             categorias ={categorias}
              />
-=======
-import React, {useState, useEffect} from 'react'
-import {editarProducto, productoId} from "../services/productoService"
-import FormProducto from "../components/FormProducto"
-import {useParams} from "react-router-dom"
-
-function EditarProducto() {
-
-    let {id} = useParams ()
-    const [value, setValue] = useState({
-        nombre:'',
-        descripcion:'',
-        precio:0,
-        stock:0,
-        fecha_vencimiento:'',
-        fotos:[],
-        id_categoria:1
-    })
-
-    const [categorias, setCategorias] = useState ([])
-    const actualizarInput = (e) => {
-        e.preventDefault()
-        setValue({...value, [e.target.name]:e.target.value})
-    }
-
-    return (
-        <div>
-            
->>>>>>> 1fa6c7c6b714c3a53758dd7c048034a54048e6be
         </div>
     )
 }
