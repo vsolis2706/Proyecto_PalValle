7849e277f05e8e5b8af497f713ffd16072c7c2d7
vsolis2706 <vsolis2706@gmail.com>
2021-06-07 15:39:43 -0500

    Update App.js
    vsolish

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Routes from "./Routes"
 import {BrowserRouter as Router, Switch} from "react-router-dom"
-<<<<<<< HEAD
  import Layout from "./components/Layout"
 import NavigationBar from "./components/NavigationBar" 
 function App () {
@@ -18,18 +17,6 @@ return(
     </Layout> 
 
     </>
-=======
-
-function App () {
-return(
-  <Router>
-    <div className="container-lg p-5">
-      <Switch>
-        <Routes/>
-      </Switch>
-    </div>
-  </Router>
->>>>>>> 1fa6c7c6b714c3a53758dd7c048034a54048e6be
   )
 } 
 
