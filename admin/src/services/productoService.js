9bbd404c5f1240cae4c652123e317adc2d230ef9
vsolis2706 <vsolis2706@gmail.com>
2021-06-07 15:42:01 -0500

    Update productoService.js

--- admin/src/services/productoService.js
+++ admin/src/services/productoService.js
@@ -69,26 +69,9 @@ const crearProducto = async (nuevoProducto) =>{
   
   }
 
-const editarProducto = async (productoEditado, id) => {
-    try {
-        let headers = {
-            "Content-Type":"application/json"
-        }
-        let {data} = await axios.put(`${URL}/${id}`, productoEditado, {headers})
-        return data
-    } catch (error) {
-        console.log(error)
-    }
-}
 
-const productoId = async (id) => {
-    try {
-        let {data} = await axios.get(`${URL}/${id}`)
-        return data
-    } catch (error) {
-        console.log(error)
-    }
-}
+
+
 
 export {
     obtenerProducto,
